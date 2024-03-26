7b8bd5830c7dbe55327bbac272d9fb18cb5806da
Nikola55-png <ilicnikola715@gmail.com>
2024-03-26 15:47:29 +0100

    Initial Commit

---
+++ ShoppingListDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingListDemo
+//
+//  Created by Nikola Ilic on 26.3.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
